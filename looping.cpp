e5f02a0a29c9393fc3a8e26be5c4c50535f4b021
ririndeismillia <ririndeismillia@gmail.com>
2026-03-30 20:33:12 +0700

    commit2

--- looping.cpp
+++ looping.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     int x;
 
-    cout<<"PERULANGAN FOR"<<endl
+    cout<<"PERULANGAN FOR"<<endl;
     for(int i=0;i<=4;i++){
         cout<<"Teknologi Informasi UMY"<<endl;
     }
@@ -17,5 +17,17 @@ int main(){
         cout<<"Bilangan acak = "<<x <<x <<endl;
         x = rand()%10;
     }
+
+    cout<<"Bilangan acak while yang terakhir ="<<x <<endl;
+    cout<<endl;
+
+    cout<<"PERULANGAN DO....WHILE"<<endl;
+    x = 1 + rand()%10;
+    do{
+        cout<<"Bilangan acak = "<<x <<endl;
+        x = rand()%10;
+    }while(x<=5);
+    cout<<"Bilangan acak do-while yang terakhir ="<<x <<endl;
+    cout<<endl;
 
 }
