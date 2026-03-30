1e1322f42f0925b2a4094cdbbf40d01cacc4070b
ririndeismillia <ririndeismillia@gmail.com>
2026-03-30 21:24:25 +0700

    commit7

--- looping.cpp
+++ looping.cpp
@@ -75,3 +75,16 @@ int main(){
     cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
 
 }
+#include <iostream>
+using namespace std;
+
+int main(){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+}
