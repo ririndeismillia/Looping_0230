9912e936724b5ab039d294b7c8e4d09044d22a0c
ririndeismillia <ririndeismillia@gmail.com>
2026-03-30 20:46:10 +0700

    commit3

--- looping.cpp
+++ looping.cpp
@@ -31,3 +31,17 @@ int main(){
     cout<<endl;
 
 }
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    float nilB,nilM,rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+}
