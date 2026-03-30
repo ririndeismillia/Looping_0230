eb4dfc6a105eec28a7c5d280d7b351ba47250eb0
ririndeismillia <ririndeismillia@gmail.com>
2026-03-30 21:19:07 +0700

    commit6

--- looping.cpp
+++ looping.cpp
@@ -53,15 +53,25 @@ int main(){
     cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;   
 
 }
-#include <iostrem>
+
+#include <iostream>
 using namespace std;
 
 int main(){
     float nilB,nilM,rerata;
-    srting status;
+    string status;
     
     cout<<"Masukkan nilai Matematika = ";
     cin>>nilM;
     cout<<"Masukkan nilai Bahasa Inggris = ";
     cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60) || (nilM >= 70))
+       status = "Lulus";
+    else
+       status = "Tidak lulus";
+    cout<<"Status kelulusan = "<<status<<" ,dengan nilai rata-rata = "<<rerata<<endl;
+
 }
